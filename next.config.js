9664cf26d30b080f4750b48283961d895b22357f
John Ruffin <john@johnruffin.io>
2025-09-18 03:27:00 -0500

    feat: initial commit next app
    BluesCityFoundation webapp initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['aws-amplify']
+  }
+}
 
-module.exports = nextConfig
+export default nextConfig
