dc59a40f9b641e7b0a732a3d0e58b8808faf3065
John Ruffin <john@johnruffin.io>
2026-03-02 11:47:17 -0600

    feat: updated OpenGraph image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,37 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+const siteUrl =
+  process.env.NEXT_PUBLIC_SITE_URL ?? "https://bluescityfoundation.org";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(siteUrl),
   title: "Blues City Foundation",
   description: "In support of the Iota Omicron Chapter of Omega Psi Phi Fraternity Inc - Clarksdale, Mississippi",
   icons: {
     icon: ['/favicon.svg', '/favicon.ico'],
     apple: '/apple-touch-icon.png',
+  },
+  openGraph: {
+    title: "Blues City Foundation",
+    description:
+      "In support of the Iota Omicron Chapter of Omega Psi Phi Fraternity Inc - Clarksdale, Mississippi",
+    url: siteUrl,
+    siteName: "Blues City Foundation",
+    images: [
+      {
+        url: "/crossroadsLogo.png",
+        alt: "Blues City Foundation logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Blues City Foundation",
+    description:
+      "In support of the Iota Omicron Chapter of Omega Psi Phi Fraternity Inc - Clarksdale, Mississippi",
+    images: ["/crossroadsLogo.png"],
   },
 };
 
